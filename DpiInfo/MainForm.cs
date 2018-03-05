aba05f5a76c4bc6a01838fa5b2b58e192a12049a
Kenneth Evans <ken@kenevans.net>
2018-03-05 11:25:48 -0500

    Handled DPI_AWARENESS, PROCESS_DPI_AWARENESS, and used AreDpiAwarenessContextsEqual in DpiAware.
    Also did other cleanup and enhancement.

--- DpiInfo/MainForm.cs
+++ DpiInfo/MainForm.cs
@@ -29,8 +29,8 @@ namespace DpiInfo {
             // Get this process
             Process currentProcess = Process.GetCurrentProcess();
             sb.Append(getProcessInfo(currentProcess));
-            sb.AppendLine("  Bounds: X=" + Bounds.Left + " Y=" + Bounds.Top
-               + " Size: " + Bounds.Width + " x " + Bounds.Height);
+            //sb.AppendLine("  Bounds: X=" + Bounds.Left + " Y=" + Bounds.Top
+            //   + " Size: " + Bounds.Width + " x " + Bounds.Height);
             sb.AppendLine();
 
             // Get all processes running on the local computer.
@@ -50,7 +50,8 @@ namespace DpiInfo {
                     }
                     sb.Append(getProcessInfo(process));
                 } catch (Exception ex) {
-                    sb.AppendLine(" *** Exception: " + ex.Message);
+                    sb.AppendLine("Process: " + process.ProcessName +
+                        " (" + process.Id + "): *** Exception: " + ex.Message);
                 }
             }
             return sb.ToString();
@@ -68,6 +69,7 @@ namespace DpiInfo {
             sb.AppendLine(String.Format("  Handle: {0} Main Window Handle: {1}",
                 process.Handle.ToString("X8"),
                 process.MainWindowHandle.ToString("X8")));
+            sb.AppendLine("  PID: " + process.Id);
 
             // 32 or 64 bit
             if (!Environment.Is64BitOperatingSystem) {
@@ -100,33 +102,42 @@ namespace DpiInfo {
                     + " Top=" + rect.Top + " Bottom=" + rect.Bottom);
             }
 
-            // Dpi awareness
-            NativeMethods.PROCESS_DPI_AWARENESS awareness;
-            int res = NativeMethods.GetProcessDpiAwareness(process.Handle,
-                out awareness);
-            if (res == NativeMethods.S_OK) {
-                sb.AppendLine("  DPI Awareness: " + awareness);
-            } else {
-                sb.AppendLine("  DPI Awareness failed: res=" + res);
-            }
-
-            // Dpi awareness context
-            IntPtr dpiAwarenessContext = NativeMethods.GetWindowDpiAwarenessContext(process.MainWindowHandle);
-            if (dpiAwarenessContext == (IntPtr)(-1)) {
-                sb.AppendLine("  DPI Awareness Context: DPI_AWARENESS_CONTEXT_UNAWARE");
-            } else if (dpiAwarenessContext == (IntPtr)(-2)) {
-                sb.AppendLine("  DPI Awareness Context: DPI_AWARENESS_CONTEXT_SYSTEM_AWARE");
-            } else if (dpiAwarenessContext == (IntPtr)(-3)) {
-                sb.AppendLine("  DPI Awareness Context: DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE");
-            } else if (dpiAwarenessContext == (IntPtr)(-4)) {
-                sb.AppendLine("  DPI Awareness Context: DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2");
-            } else {
-                sb.AppendLine("  DPI Awareness Context failed: " + dpiAwarenessContext);
-            }
-
             // Dpi
             int dpi = NativeMethods.GetDpiForWindow(process.MainWindowHandle);
             sb.AppendLine("  DPI: " + dpi);
+
+            // Process DPI awareness
+            NativeMethods.PROCESS_DPI_AWARENESS processDpiAwareness;
+            int res = NativeMethods.GetProcessDpiAwareness(process.Handle,
+                out processDpiAwareness);
+            if (res == NativeMethods.S_OK) {
+                sb.AppendLine("  Process DPI Awareness: " + processDpiAwareness);
+            } else {
+                sb.AppendLine("  Process DPI Awareness failed: res=" + res);
+            }
+
+            // DPI awareness context
+            IntPtr dpiAwarenessContext = NativeMethods.GetWindowDpiAwarenessContext(process.MainWindowHandle);
+            if (NativeMethods.AreDpiAwarenessContextsEqual(dpiAwarenessContext, (IntPtr)(-1))) {
+                sb.AppendLine("  DPI Awareness Context: (" + dpiAwarenessContext +
+                    ") DPI_AWARENESS_CONTEXT_UNAWARE");
+            } else if (NativeMethods.AreDpiAwarenessContextsEqual(dpiAwarenessContext, (IntPtr)(-2))) {
+                sb.AppendLine("  DPI Awareness Context: (" + dpiAwarenessContext +
+                    ") DPI_AWARENESS_CONTEXT_SYSTEM_AWARE");
+            } else if (NativeMethods.AreDpiAwarenessContextsEqual(dpiAwarenessContext, (IntPtr)(-3))) {
+                sb.AppendLine("  DPI Awareness Context: (" + dpiAwarenessContext +
+                    ") DPI_AWARENESS_CONTEXT_SYSTEM_AWARE");
+            } else if (NativeMethods.AreDpiAwarenessContextsEqual(dpiAwarenessContext, (IntPtr)(-4))) {
+                sb.AppendLine("  DPI Awareness Context: (" + dpiAwarenessContext +
+                    ") DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2");
+            } else {
+                sb.AppendLine("  DPI Awareness Context: " + dpiAwarenessContext);
+            }
+
+            // DPI awareness
+            NativeMethods.DPI_AWARENESS dpiAwareness =
+            (NativeMethods.DPI_AWARENESS)NativeMethods.GetAwarenessFromDpiAwarenessContext(dpiAwarenessContext);
+            sb.AppendLine("  DPI Awareness: " + dpiAwareness);
 
             return sb.ToString();
         }
@@ -182,10 +193,18 @@ namespace DpiInfo {
     /// </summary>
     internal static class NativeMethods {
         internal const int S_OK = 0;
+
         internal enum PROCESS_DPI_AWARENESS {
             PROCESS_DPI_UNAWARE = 0,
             PROCESS_SYSTEM_DPI_AWARE = 1,
             PROCESS_PER_MONITOR_DPI_AWARE = 2
+        }
+
+        internal enum DPI_AWARENESS {
+            DPI_AWARENESS_INVALID = -1,
+            DPI_AWARENESS_UNAWARE = 0,
+            DPI_AWARENESS_SYSTEM_AWARE = 1,
+            DPI_AWARENESS_PER_MONITOR_AWARE = 2
         }
 
         [DllImport("SHcore.dll")]
@@ -197,7 +216,16 @@ namespace DpiInfo {
         [DllImport("user32.dll")]
         internal static extern IntPtr GetWindowDpiAwarenessContext(IntPtr hWnd);
 
+        [DllImport("user32.dll")]
+        internal static extern int GetAwarenessFromDpiAwarenessContext(IntPtr dpiContext);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool AreDpiAwarenessContextsEqual(IntPtr dpiContextA,
+            IntPtr dpiContextB);
+
         [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
         static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
 
         [DllImport("user32.dll")]
